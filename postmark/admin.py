27dc0cabe3a8ea38337cffd23e15ce69f07a8f5a
dstufft <donald.stufft@gmail.com>
2011-05-23 03:38:28 -0400

    use fieldsets to pretty up the display information and readonly_fields to prevent modificaiton

--- postmark/admin.py
+++ postmark/admin.py
@@ -6,5 +6,25 @@ class EmailMessageAdmin(admin.ModelAdmin):
     list_display = ("to", "to_type", "subject", "tag", "status", "submitted_at")
     list_filter = ("status", "tag", "to_type", "submitted_at")
     search_fields = ("message_id", "to", "subject")
+    
+    readonly_fields = ("message_id", "status", "subject", "tag", "to", "to_type", "sender", "reply_to", "submitted_at", "text_body", "html_body", "headers", "attachments")
+    
+    fieldsets = (
+        (None, {
+            "fields": ("message_id", "status", "subject", "tag", "to", "to_type", "sender", "reply_to", "submitted_at")
+        }),
+        ("Text Body", {
+            "fields": ("text_body",),
+            "classes": ("collapse", "closed")
+        }),
+        ("HTML Body", {
+            "fields": ("html_body",),
+            "classes": ("collapse", "closed")
+        }),
+        ("Advanced", {
+            "fields": ("headers", "attachments"),
+            "classes": ("collapse", "closed")
+        })
+    )
 
 admin.site.register(EmailMessage, EmailMessageAdmin)
